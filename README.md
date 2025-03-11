99d124a13f0cd0e74cd37185d0b75842150c659c
Ryan <ryan.lewalski@gmail.com>
2025-03-11 14:19:22 -0700

    Begin work on models.py, added static and templates.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # time_managment
 A web app that will store data about when someone has clocked in and out of a work schedule.
+
+
+Instructions/Documentation:
+
+This web app will follow a Django structure.
+The user will be able to login to an account and after loggin in be able to choose from a few different options.
+Once logged in, the user will be able to see a select one of two options
+(Thinking about doing one option for manual tracking, and a second option for start and end tracking) - need to ask dad
+
---
+++ time_management/core/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class User(models.Model):
+    name = models.TextField()
+    email = models.EmailField(unique=True)
+    password_hash = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Session(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
